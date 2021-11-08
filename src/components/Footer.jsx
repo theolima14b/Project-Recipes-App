e87bbeec312e06490547f1d6d68dcd2039b267ab
Elithon Silva <elithonsilva@gmail.com>
2021-11-08 16:44:04 -0300

    add Link de rotas aos icones correspondentes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
@@ -6,24 +7,30 @@ import mealIcon from '../images/mealIcon.svg';
 function Footer() {
   return (
     <footer data-testid="footer">
-      <img
-        height={ 30 }
-        src={ drinkIcon }
-        alt="Drink Icon"
-        data-testid="drinks-bottom-btn"
-      />
-      <img
-        height={ 30 }
-        src={ exploreIcon }
-        alt="Explore Icon"
-        data-testid="explore-bottom-btn"
-      />
-      <img
-        height={ 30 }
-        src={ mealIcon }
-        alt="Meal Icon"
-        data-testid="food-bottom-btn"
-      />
+      <Link to="/bebidas">
+        <img
+          height={ 30 }
+          src={ drinkIcon }
+          alt="Drink Icon"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          height={ 30 }
+          src={ exploreIcon }
+          alt="Explore Icon"
+          data-testid="explore-bottom-btn"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          height={ 30 }
+          src={ mealIcon }
+          alt="Meal Icon"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
     </footer>
   );
 }
