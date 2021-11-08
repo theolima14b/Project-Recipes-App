468977ffda37aa1319b91bf66af3ed8d2a314607
Elithon Silva <elithonsilva@gmail.com>
2021-11-08 15:56:25 -0300

    adiciona icones drink, explore and meal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
-    <footer>
-
+    <footer data-testid="footer">
+      <img
+        height={ 30 }
+        src={ drinkIcon }
+        alt="Drink Icon"
+        data-testid="drinks-bottom-btn"
+      />
+      <img
+        height={ 30 }
+        src={ exploreIcon }
+        alt="Explore Icon"
+      />
+      <img
+        height={ 30 }
+        src={ mealIcon }
+        alt="Meal Icon"
+      />
     </footer>
   );
 }
