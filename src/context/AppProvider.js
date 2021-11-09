7f097f58b0d2cf87d9f8220140c97141cfa7c17f
Elithon Silva <elithonsilva@gmail.com>
2021-11-09 09:59:15 -0300

    exclui arquivo routers

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
-  const [state, setstate] = useState(initialState);
+  // const [state, setstate] = useState(initialState);
 
   const stateDefault = {};
   return (
