4e75b2e6e52a950a2eb14fdd92c4ebe95fd57f99
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-11-09 19:40:07 -0300

    resolvendo conflitos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import AppProvider from './context/AppProvider';
 import Switcher from './components/Switcher';
 
 function App() {
   return (
     <main className="main">
-      <span>HomePage</span>
-      <BrowserRouter>
-        <Switcher />
-      </BrowserRouter>
+      <AppProvider>
+        <span>HomePage</span>
+        <BrowserRouter>
+          <Switcher />
+        </BrowserRouter>
+      </AppProvider>
     </main>
   );
 }
