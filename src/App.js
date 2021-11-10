44017c882e7d00073ca781ff73757081a9fbc04d
Elithon Silva <elithonsilva@gmail.com>
2021-11-10 15:20:48 -0300

    add importação App.css que foi removido por engano

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 import AppProvider from './context/AppProvider';
 import Switcher from './components/Switcher';
 
