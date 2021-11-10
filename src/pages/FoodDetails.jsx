b990c4d8791eca4927ff9b21caafc20fecc0614d
Elithon Silva <elithonsilva@gmail.com>
2021-11-10 16:06:10 -0300

    chamada de api

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import AppContext from '../context/AppContext';
 import HeaderRecipes from '../components/details recipes/HeaderRecipes';
 import SectionRecipes from '../components/details recipes/SectionRecipes';
 
-function FoodDetails() {
+function FoodDetails(props) {
+  const { match: { params: { id } } } = props;
+  const { setDetailsPage } = useContext(AppContext);
+
+  useEffect(() => {
+    async function getAPI() {
+      const idDaReceita = id;
+      console.log(id);
+      const responseAPI = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idDaReceita}`);
+      const results = await responseAPI.json();
+      const foodDetailsResults = results.meals[0];
+    }
+    getAPI();
+  });
+
   return (
     <main>
       <HeaderRecipes />
@@ -10,4 +25,5 @@ function FoodDetails() {
     </main>
   );
 }
+
 export default FoodDetails;
