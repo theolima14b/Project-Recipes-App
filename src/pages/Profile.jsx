6fc4d8eff005e6675a7618e62b87777f84b2f66f
enzo <enzo@pop-os.localdomain>
2021-11-10 15:55:44 -0300

    implementação profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+  useEffect(() => {
+    const storage = JSON.parse(localStorage.getItem('user'));
+    setEmail(storage.email);
+  }, []);
+
+  const history = useHistory();
+
+  const redirect = (route) => {
+    history.push(route);
+  };
+
+  const logOut = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <main>
       <Header title="Perfil" bool={ false } />
       Profile
+      <p data-testid="profile-email">{email}</p>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => redirect('/receitas-feitas') }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => redirect('/receitas-favoritas') }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ () => logOut() }
+      >
+        Sair
+      </button>
       <Footer />
     </main>
   );
