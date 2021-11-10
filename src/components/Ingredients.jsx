9befbc35a4f63914ffc2eb34765de735ef7f05e1
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-11-10 17:04:39 -0300

    componente Ingredients

---
+++ src/components/Ingredients.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CheckBox from './CheckBox';
+
+function Ingredients({ recipe }) {
+  const arrayRecipe = recipe[0];
+  const keys = Object.keys(arrayRecipe);
+  const values = Object.values(arrayRecipe);
+  let igredientes = [];
+  keys.forEach((key, index) => {
+    if (key.includes('strIngredient') && values[index] !== '') {
+      igredientes = [...igredientes, values[index]];
+    }
+  });
+
+  return (
+    <main>
+      <h3>Ingredients</h3>
+      <ol>
+        { igredientes.map((string, index) => (
+          <li key={ index }>
+            <CheckBox />
+            {string}
+          </li>)) }
+      </ol>
+    </main>
+  );
+}
+
+Ingredients.propTypes = {
+  recipe: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
+
+export default Ingredients;
