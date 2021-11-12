7b7696b721d446ce12a8979d57b1139908c304d5
GustavoPD <gudalmoro@gmail.com>
2021-11-11 21:49:36 -0300

    Requisitos 75 e 76

--- src/pages/FoodsByIngredients.jsx
+++ src/pages/FoodsByIngredients.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import IngredientsCard from '../components/IngredientsCard';
 
 function FoodsByIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  async function fetchIngredients() {
+    const URL = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+    const response = await fetch(URL);
+    const listOfIngredients = await response.json();
+    setIngredients(listOfIngredients.meals);
+  }
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
+  const limitArray = 12;
+  const arrayIngredients = ingredients.slice(0, limitArray);
+
   return (
     <main>
       <Header title="Explorar Ingredientes" bool={ false } />
-      FoodsByIngredients
+      {arrayIngredients.map((ingredient, index) => (
+        <IngredientsCard
+          key={ ingredient.strIngredient }
+          index={ index }
+          name={ ingredient.strIngredient }
+          image={
+            `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`
+          }
+        />
+      ))}
       <Footer />
     </main>
   );
 }
+
 export default FoodsByIngredients;
