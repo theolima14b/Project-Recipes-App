c5d7f12b72ae93670d5eff1cbd9a1ce463ec99fb
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-11-14 21:30:15 -0300

    corrige req 32

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,22 +1,23 @@
-import React, { useContext } from 'react';
+import React from 'react';
+// import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import useFetchRecipeDetails from '../hooks/useFetchRecipeDetails';
-import useFetchRecomendacoes from '../hooks/useFetchRecomendacoes';
 import HeaderRecipes from '../components/details recipes/HeaderRecipes';
-import Instructions from '../components/details recipes/Instructions';
-import Ingredients from '../components/details recipes/Ingredients';
-import CardRecipe from '../components/CardRecipe';
-import AppContext from '../context/AppContext';
+// import useFetchRecipeDetails from '../hooks/useFetchRecipeDetails';
+// import useFetchRecomendacoes from '../hooks/useFetchRecomendacoes';
+// import Instructions from '../components/details recipes/Instructions';
+// import Ingredients from '../components/details recipes/Ingredients';
+// import CardRecipe from '../components/CardRecipe';
+// import AppContext from '../context/AppContext';
 
-function DrinkDetails(props) {
-  const { detailsPage, recomendacoes } = useContext(AppContext);
-  const { match: { params: { id } } } = props;
-  const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
-  const drinks = 'drinks';
-  useFetchRecipeDetails(drinkURL, drinks);
+function DrinkDetails() {
+  // const { detailsPage, recomendacoes } = useContext(AppContext);
+  // const { match: { params: { id } } } = props;
+  // const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+  // const drinks = 'drinks';
+  // useFetchRecipeDetails(drinkURL, drinks);
 
-  const recommendationsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-  useFetchRecomendacoes(recommendationsURL, drinks);
+  // const recommendationsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  // useFetchRecomendacoes(recommendationsURL, drinks);
 
   return (
     <main>
