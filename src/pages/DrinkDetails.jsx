3205d465c915975ec4e3ee18aa5ef3816cebbe67
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-11-14 23:20:25 -0300

    ultimo commit

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-// import React, { useContext } from 'react';
-// import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import useFetchRecipeDetails from '../hooks/useFetchRecipeDetails';
+import useFetchRecomendacoes from '../hooks/useFetchRecomendacoes';
 import HeaderRecipes from '../components/details recipes/HeaderRecipes';
-// import useFetchRecipeDetails from '../hooks/useFetchRecipeDetails';
-// import useFetchRecomendacoes from '../hooks/useFetchRecomendacoes';
-// import Instructions from '../components/details recipes/Instructions';
-// import Ingredients from '../components/details recipes/Ingredients';
-// import CardRecipe from '../components/CardRecipe';
-// import AppContext from '../context/AppContext';
+import Instructions from '../components/details recipes/Instructions';
+import Ingredients from '../components/details recipes/Ingredients';
+import CardRecipe from '../components/CardRecipe';
+import AppContext from '../context/AppContext';
 
-function DrinkDetails() {
-  // const { detailsPage, recomendacoes } = useContext(AppContext);
-  // const { match: { params: { id } } } = props;
-  // const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
-  // const drinks = 'drinks';
-  // useFetchRecipeDetails(drinkURL, drinks);
-  // const recommendationsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-  // useFetchRecomendacoes(recommendationsURL, drinks);
+function DrinkDetails(props) {
+  const { detailsPage, recomendacoes } = useContext(AppContext);
+  const { match: { params: { id } } } = props;
+  const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const drinks = 'drinks';
+  useFetchRecipeDetails(drinkURL, drinks);
+
+  const recommendationsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  useFetchRecomendacoes(recommendationsURL, drinks);
 
   return (
     <main>
       <HeaderRecipes type="Drink" />
-      {/* <Instructions />
+      <Instructions />
       <Ingredients recipe={ detailsPage } />
       {recomendacoes.map((drink, index) => (
         <CardRecipe
@@ -34,13 +34,13 @@ function DrinkDetails() {
           bool
         />
       ))}
-      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button> */}
+      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
     </main>
   );
 }
 
-// DrinkDetails.propTypes = {
-//   match: PropTypes.objectOf(PropTypes.any).isRequired,
-// };
+DrinkDetails.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default DrinkDetails;
